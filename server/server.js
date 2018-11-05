31469a839a0dd548535c5a540bc7431a872e6b84
martin gardner II <martingardnerii@gmail.com>
2018-11-05 15:28:57 -0600

    add in /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -35,6 +36,29 @@ app.get('/todos', (req, res) => {
 			});
 		}, (e) => {
 			res.status(400).send(e);
+		});
+});
+
+app.get('/todos/:id', (req, res) => {
+	//res.send(req.params);
+	let id = req.params.id;
+	if( !ObjectID.isValid(id) ){
+		//console.log('ID is not valid');
+		return res.status(404).send();
+	}
+
+	Todo
+		.findById(id)
+		.then( (todo) => {
+			if( !todo ){
+				return res.status(404).send();
+			}
+			return res.send({
+				todo : todo
+			});
+		});
+		.catch( (e) => {
+			return res.status(400).send();
 		});
 });
 
